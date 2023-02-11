b9fcec4283804f5093587dda68a078f140355a83
Sam Leighton <sam@samleighton.us>
2023-02-10 16:34:14 -0800

    Disabled caching on all pages.

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,17 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  headers: () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store',
+        },
+      ],
+    },
+  ],
 }
 
 export default withMDX(nextConfig)
