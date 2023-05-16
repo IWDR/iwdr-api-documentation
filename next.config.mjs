edeccab5eef18214d66b15edcee03340de522368
Sam Leighton <sam@samleighton.us>
2023-05-16 13:39:44 -0700

    Disabled caching on protected pages.

--- next.config.mjs
+++ next.config.mjs
@@ -12,10 +12,74 @@ const withMDX = nextMDX({
     },
 })
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md']
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
+    headers: () => [
+        {
+            source: '/resources/:path*',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+        {
+            source: '/login',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+        {
+            source: '/tokens',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+        {
+            source: '/token-application',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+        {
+            source: '/authentication',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+        {
+            source: '/mapping',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+        {
+            source: '/errors',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store'
+                }
+            ]
+        },
+    ]
 }
 
 export default withMDX(nextConfig)
