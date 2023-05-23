7db5d047852601b314549e38cbbc1a463be0c110
Sam Leighton <sam@samleighton.us>
2023-05-22 23:06:53 -0700

    Added coat color data tables.

---
+++ src/components/data-tables/CoatColorDataTable.jsx
@@ -0,0 +1,49 @@
+import {useState} from "react";
+import {Button} from "@/components/Button";
+import {Transition} from "@headlessui/react";
+import {DataTable} from "@/components/DataTable";
+
+export default function CoatColorDataTable(){
+    const [display, setDisplay] = useState(false);
+
+    const headers = [
+        {
+            key: 'dbcc_BreedColorCode',
+            text: 'Unique Identifier'
+        },
+        {
+            key: 'dbcc_ColorDescription',
+            text: 'Display Name'
+        },
+        {
+            key: 'dbc_DogBreedDescription',
+            text: 'Breed Name'
+        }
+    ];
+
+    return (
+        <>
+            <Button onClick={() => setDisplay(!display)}>
+                {display
+                    ? 'Hide Available Coat Colors'
+                    : 'Show Available Coat Colors'}
+            </Button>
+            <Transition
+                show={display}
+                enter="transition-opacity duration-150"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-150"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <DataTable
+                    path='/api/references/coat-color'
+                    headers={headers}
+                    paginated
+                    searchable
+                />
+            </Transition>
+        </>
+    )
+}
