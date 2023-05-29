f491f813b9105dd17abded9aa13bed0de0f7978f
Sam Leighton <sam@samleighton.us>
2023-05-29 10:24:31 -0700

    Damn you useEffect

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -43,7 +43,7 @@ export default function App({Component, pageProps: {...pageProps}}) {
         return () => {
             router.events.off('routeChangeComplete', onRouteChangeComplete);
         }
-    })
+    }, []);
 
     return (
         <>
