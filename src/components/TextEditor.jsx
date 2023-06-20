790eeb4dd3efeff3648f700c2da4719b2c75bc36
armorokeson <waokeson@gmail.com>
2023-06-20 16:55:00 -0400

    Note that support is not ready

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,37 +1,42 @@
-import {forwardRef} from "react";
-import {Editor} from "@tinymce/tinymce-react";
+import { forwardRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
-export const TextEditor = forwardRef(function TextEditor({
-                                                             height,
-                                                             menubar,
-                                                             placeholder,
-                                                             name,
-                                                             id,
-                                                             onChange,
-                                                             className,
-                                                             label,
-                                                             help,
-                                                             disabled = false,
-                                                             required = false,
-                                                             horizontal = false
-                                                         }, ref) {
-    const mce_plugins = 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tableofcontents footnotes mergetags autocorrect typography inlinecss';
-    const mce_toolbar = 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat'
+export const TextEditor = forwardRef(function TextEditor(
+    {
+        height,
+        menubar,
+        placeholder,
+        name,
+        id,
+        onChange,
+        className,
+        label,
+        help,
+        disabled = false,
+        required = false,
+        horizontal = false,
+    },
+    ref
+) {
+    const mce_plugins =
+        'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount';
+    const mce_toolbar =
+        'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat';
 
     return (
         <div className="my-4">
             <div>
                 <label
                     htmlFor={name}
-                    className="block text-sm font-semibold leading-6 text-zinc-900 dark:text-white my-2"
+                    className="my-2 block text-sm font-semibold leading-6 text-zinc-900 dark:text-white"
                 >
                     {label}
-                    {required && <span className="text-red-600 ml-0.5">*</span>}
-                    {help && <p className="text-sm font-medium max-w-sm my-0.5">{help}</p>}
+                    {required && <span className="ml-0.5 text-red-600">*</span>}
+                    {help && <p className="my-0.5 max-w-sm text-sm font-medium">{help}</p>}
                 </label>
                 <Editor
                     apiKey={process.env.NEXT_PUBLIC_TINYMCE_API_KEY}
-                    onInit={(evt, editor) => ref.current = editor}
+                    onInit={(evt, editor) => (ref.current = editor)}
                     initialValue={placeholder}
                     init={{
                         height: height,
@@ -40,12 +45,12 @@ export const TextEditor = forwardRef(function TextEditor({
                         toolbar: mce_toolbar,
                         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
                         content_css: 'default',
-                        skin: 'oxide'
+                        skin: 'oxide',
                     }}
                     disabled={disabled}
                     id={id}
                 />
             </div>
         </div>
-    )
+    );
 });
--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -1,18 +1,26 @@
-import {SupportForm} from "@/components/forms/SupportForm";
+import { SupportForm } from '@/components/forms/SupportForm';
+import { Note } from '@/components/mdx';
 
 export function getServerSideProps() {
     return {
         props: {
-            title: 'Support Ticket'
-        }
-    }
+            title: 'Support Ticket',
+        },
+    };
 }
 
 export default function Support() {
     return (
         <>
+            <Note alert>
+                This page is currently a work in progress and is not fully functional. If you need to submit a support
+                ticket please do so on the IWDR database interface at{' '}
+                <a href="https://iwdr.org/iwdr" target="_blank" rel="noreferrer">
+                    https://iwdr.org/iwdr
+                </a>
+            </Note>
             <h1>Submit a support ticket</h1>
-            <SupportForm/>
+            <SupportForm />
         </>
     );
 }
