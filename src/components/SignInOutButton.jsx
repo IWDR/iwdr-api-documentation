ec0875d6f896b8c72224fc7080286319a2e86f29
Sam Leighton <sam@samleighton.us>
2023-10-02 16:11:21 -0700

    Force page reload on sign out

--- src/components/SignInOutButton.jsx
+++ src/components/SignInOutButton.jsx
@@ -1,17 +1,26 @@
 import { Button } from '@/components/Button';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function SignInOutButton({ className }) {
     const { data: session } = useSession();
+    const router = useRouter();
+
+    // Custom logout function because next-auth
+    // does not refresh page for some reason.
+    const logout = async () => {
+        await signOut();
+        router.reload();
+    };
 
     return (
         <>
             {!!session?.user ? (
-                <Button onClick={() => signOut()} className={className}>
+                <Button onClick={() => logout()} className={className}>
                     Sign out
                 </Button>
             ) : (
-                <Button onClick={() => signIn('iwdr', { redirect: true, callbackUrl: '/' })} className={className}>
+                <Button onClick={() => signIn('iwdr')} className={className}>
                     Sign In with IWDR
                 </Button>
             )}
