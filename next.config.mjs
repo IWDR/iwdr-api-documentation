6c5b47aacb0d356ff9f641527ce15a71d12e4e27
Sam Leighton <sam@samleighton.us>
2024-09-07 20:49:28 -0700

    Added cors to api routes.

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,19 @@ const withMDX = nextMDX({
 const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_API_URL }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "*" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 }
 
 export default withMDX(nextConfig)
